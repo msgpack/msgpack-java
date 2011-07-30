39c7fa7f5e5a0899cc088af87c2902447f7ffb00
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-07-31 04:45:51 +0900

    renamed test programs v2

---
+++ src/test/java/org/msgpack/TestPackBufferUnpack.java
@@ -0,0 +1,265 @@
+package org.msgpack;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.msgpack.packer.Packer;
+import org.msgpack.unpacker.BufferUnpacker;
+
+
+public class TestPackBufferUnpack extends TestSet {
+
+    @Test @Override
+    public void testBoolean() throws Exception {
+	super.testBoolean();
+    }
+
+    @Override
+    public void testBoolean(boolean v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeBoolean(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	boolean ret = unpacker.readBoolean();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testByte() throws Exception {
+	super.testByte();
+    }
+
+    @Override
+    public void testByte(byte v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeByte(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	byte ret = unpacker.readByte();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testShort() throws Exception {
+	super.testShort();
+    }
+
+    @Override
+    public void testShort(short v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeShort(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	short ret = unpacker.readShort();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testInteger() throws Exception {
+	super.testInteger();
+    }
+
+    @Override
+    public void testInteger(int v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeInt(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	int ret = unpacker.readInt();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testLong() throws Exception {
+	super.testLong();
+    }
+
+    @Override
+    public void testLong(long v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeLong(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	long ret = unpacker.readLong();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testFloat() throws Exception {
+	super.testFloat();
+    }
+
+    @Override
+    public void testFloat(float v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeFloat(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	float ret = unpacker.readFloat();
+	assertEquals(v, ret, 10e-10);
+    }
+
+    @Test @Override
+    public void testDouble() throws Exception {
+	super.testDouble();
+    }
+
+    @Override
+    public void testDouble(double v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeDouble(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	double ret = unpacker.readDouble();
+	assertEquals(v, ret, 10e-10);
+    }
+
+    @Test @Override
+    public void testNil() throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeNil();
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	unpacker.readNil();
+    }
+
+    @Test @Override
+    public void testBigInteger() throws Exception {
+	super.testBigInteger();
+    }
+
+    @Override
+    public void testBigInteger(BigInteger v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeBigInteger(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	BigInteger ret = unpacker.readBigInteger();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testString() throws Exception {
+	super.testString();
+    }
+
+    @Override
+    public void testString(String v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeString(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	String ret = unpacker.readString();
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testByteArray() throws Exception {
+	super.testByteArray();
+    }
+
+    @Override
+    public void testByteArray(byte[] v) throws Exception {
+	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeByteArray(v);
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	byte[] ret = unpacker.readByteArray();
+	assertArrayEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testList() throws Exception {
+	super.testList();
+    }
+
+    @Override
+    public <E> void testList(List<E> v, Class<E> elementClass) throws Exception {
+    	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeArrayBegin(v.size());
+	for (Object o : v) {
+	    packer.write(o);
+	}
+	packer.writeArrayEnd();
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	int size = unpacker.readArrayBegin();
+	List ret = new ArrayList(size);
+	for (int i = 0; i < size; ++i) {
+	    ret.add(unpacker.read(elementClass));
+	}
+	unpacker.readArrayEnd();
+	assertEquals(v.size(), ret.size());
+	Iterator v_iter = v.iterator();
+	Iterator ret_iter = ret.iterator();
+	while (v_iter.hasNext()) {
+	    assertEquals(v_iter.next(), ret_iter.next());
+	}
+    }
+
+    @Test @Override
+    public void testMap() throws Exception {
+	super.testMap();
+    }
+
+    @Override
+    public <K, V> void testMap(Map<K, V> v, Class<K> keyElementClass, Class<V> valueElementClass) throws Exception {
+    	MessagePack msgpack = new MessagePack();
+	ByteArrayOutputStream out = new ByteArrayOutputStream();
+	Packer packer = msgpack.createPacker(out);
+	packer.writeMapBegin(v.size());
+	for (Map.Entry<Object, Object> e : ((Map<Object, Object>) v).entrySet()) {
+	    packer.write(e.getKey());
+	    packer.write(e.getValue());
+	}
+	packer.writeMapEnd();
+	byte[] bytes = out.toByteArray();
+	BufferUnpacker unpacker = msgpack.createBufferUnpacker(bytes);
+	int size = unpacker.readMapBegin();
+	Map ret = new HashMap(size);
+	for (int i = 0; i < size; ++i) {
+	    Object key = unpacker.read(keyElementClass);
+	    Object value = unpacker.read(valueElementClass);
+	    ret.put(key, value);
+	}
+	unpacker.readMapEnd();
+	assertEquals(v.size(), ret.size());
+	for (Map.Entry<Object, Object> e : ((Map<Object, Object>) v).entrySet()) {
+	    Object value = ret.get(e.getKey());
+	    assertEquals(e.getValue(), value);
+	}
+    }
+}
---
+++ src/test/java/org/msgpack/template/builder/TestReflectionOrdinalEnumBufferPackUnpack.java
@@ -0,0 +1,58 @@
+package org.msgpack.template.builder;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Test;
+import org.msgpack.MessagePack;
+import org.msgpack.packer.BufferPacker;
+import org.msgpack.template.TemplateRegistry;
+import org.msgpack.template.Template;
+import org.msgpack.testclasses.EnumTypeFieldsClass;
+import org.msgpack.testclasses.EnumTypeFieldsClassNotNullable;
+import org.msgpack.unpacker.Unpacker;
+
+
+public class TestReflectionOrdinalEnumBufferPackUnpack extends TestSet {
+
+    @Test @Override
+    public void testEnumTypeFieldsClass() throws Exception {
+	super.testEnumTypeFieldsClass();
+    }
+
+    @Override
+    public void testEnumTypeFieldsClass(EnumTypeFieldsClass v) throws Exception {
+	TemplateRegistry registry = new TemplateRegistry();
+	registry.register(EnumTypeFieldsClass.SampleEnum.class,
+		new ReflectionOrdinalEnumTemplateBuilder(registry).buildTemplate(EnumTypeFieldsClass.SampleEnum.class));
+	ReflectionTemplateBuilder builder = new ReflectionTemplateBuilder(registry);
+	Template<EnumTypeFieldsClass> tmpl = builder.buildTemplate(EnumTypeFieldsClass.class);
+	BufferPacker packer = new MessagePack().createBufferPacker();
+	tmpl.write(packer, v);
+	byte[] bytes = packer.toByteArray();
+	Unpacker unpacker = new MessagePack().createUnpacker(new ByteArrayInputStream(bytes));
+	EnumTypeFieldsClass ret = tmpl.read(unpacker, null);
+	assertEquals(v, ret);
+    }
+
+    @Test @Override
+    public void testEnumTypeFieldsClassNotNullable() throws Exception {
+	super.testEnumTypeFieldsClassNotNullable();
+    }
+
+    @Override
+    public void testEnumTypeFieldsClassNotNullable(EnumTypeFieldsClassNotNullable v) throws Exception {
+	TemplateRegistry registry = new TemplateRegistry();
+	registry.register(EnumTypeFieldsClassNotNullable.SampleEnum.class,
+		new ReflectionOrdinalEnumTemplateBuilder(registry).buildTemplate(EnumTypeFieldsClassNotNullable.SampleEnum.class));
+	ReflectionTemplateBuilder builder = new ReflectionTemplateBuilder(registry);
+	Template<EnumTypeFieldsClassNotNullable> tmpl = builder.buildTemplate(EnumTypeFieldsClassNotNullable.class);
+	BufferPacker packer = new MessagePack().createBufferPacker();
+	tmpl.write(packer, v);
+	byte[] bytes = packer.toByteArray();
+	Unpacker unpacker = new MessagePack().createUnpacker(new ByteArrayInputStream(bytes));
+	EnumTypeFieldsClassNotNullable ret = tmpl.read(unpacker, null);
+	assertEquals(v, ret);
+    }
+}
