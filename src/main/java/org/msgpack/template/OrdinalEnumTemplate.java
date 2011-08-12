b6748c3ab5e6c6fdbe20053467de9654e574ca62
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-08-12 11:36:13 +0900

    reverted OrdinalEnumTemplate.java to original one

--- src/main/java/org/msgpack/template/OrdinalEnumTemplate.java
+++ src/main/java/org/msgpack/template/OrdinalEnumTemplate.java
@@ -18,6 +18,7 @@
 package org.msgpack.template;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.msgpack.MessageTypeException;
 import org.msgpack.packer.Packer;
@@ -25,24 +26,32 @@ import org.msgpack.unpacker.Unpacker;
 
 
 public class OrdinalEnumTemplate<T> extends AbstractTemplate<T> {
-    private T[] entries;
+    protected T[] entries;
+    protected HashMap<T, Integer> reverse;
 
     public OrdinalEnumTemplate(Class<T> targetClass) {
 	entries = targetClass.getEnumConstants();
+        reverse = new HashMap<T, Integer>();
+        for(int i = 0; i < entries.length; i++) {
+                reverse.put(entries[i], i);
+        }
     }
 
-    @SuppressWarnings("rawtypes")
     @Override
     public void write(Packer pk, T target, boolean required) throws IOException {
-	pk.writeInt(((Enum) target).ordinal());
+	Integer ordinal = reverse.get(target);
+        if(ordinal == null) {
+            throw new MessageTypeException(new IllegalArgumentException("ordinal: " + ordinal));
+        }
+        pk.writeInt((int) ordinal);
     }
 
     @Override
     public T read(Unpacker pac, T to, boolean required) throws IOException, MessageTypeException {
-	int ordinal = pac.readInt();
-	if (ordinal >= entries.length) {
-	    throw new MessageTypeException("illegal ordinal");
-	}
-	return entries[ordinal];
+        int ordinal = pac.readInt();
+        if(entries.length <= ordinal) {
+                throw new MessageTypeException(new IllegalArgumentException("ordinal: " + ordinal));
+        }
+        return entries[ordinal];
     }
 }
