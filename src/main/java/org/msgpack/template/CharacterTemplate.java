98f8fc22a13da62be07c90f06c1dd87c1b227b71
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-08-17 13:32:23 +0900

    added copyright to CharacterTemplate.java and refined it

--- src/main/java/org/msgpack/template/CharacterTemplate.java
+++ src/main/java/org/msgpack/template/CharacterTemplate.java
@@ -1,9 +1,27 @@
+//
+// MessagePack for Java
+//
+// Copyright (C) 2009-2011 FURUHASHI Sadayuki
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
 package org.msgpack.template;
 
 import java.io.IOException;
 import org.msgpack.MessageTypeException;
 import org.msgpack.packer.Packer;
 import org.msgpack.unpacker.Unpacker;
+
 
 /**
  * CharacterTemplate<br/>
@@ -15,15 +33,15 @@ public class CharacterTemplate extends AbstractTemplate<Character> {
     private CharacterTemplate() { }
 
     @Override
-    public void write(Packer pk, Character v, boolean required) throws IOException {
-        if (v == null) {
+    public void write(Packer pk, Character target, boolean required) throws IOException {
+        if (target == null) {
             if (required) {
                 throw new MessageTypeException("Attempted to write null");
             }
             pk.writeNil();
             return;
         }
-        pk.writeInt(v.charValue());
+        pk.writeInt((char) target);
     }
 
     @Override
@@ -31,7 +49,7 @@ public class CharacterTemplate extends AbstractTemplate<Character> {
         if (!required && u.trySkipNil()) {
             return null;
         }
-        return  (char) u.readInt();
+        return (char) u.readInt();
     }
 
     static public CharacterTemplate getInstance() {
