9c59663bce815465445bfa90ad3448432bda7087
Muga Nishizawa <muga.nishizawa@gmail.com>
2011-10-08 13:50:16 +0900

    modified template builders for serializing private fields in user-defined classes

--- src/main/java/org/msgpack/template/builder/DefaultBuildContext.java
+++ src/main/java/org/msgpack/template/builder/DefaultBuildContext.java
@@ -110,11 +110,12 @@ public class DefaultBuildContext extends BuildContext<FieldEntry> {
 		if (!isPrivate) {
 		    buildString("  $1.%s(_$$_t.%s);\n", primitiveWriteName(type), de.getName());
 		} else {
-		    buildString("  %s.writePrivateField($1, _$$_t, \"%s\", templates[%d]);\n",
-			    DefaultBuildContext.class.getName(), de.getName(), i);
+		    buildString("  %s.writePrivateField($1, _$$_t, %s.class, \"%s\", templates[%d]);\n",
+			    DefaultBuildContext.class.getName(), de.getField().getDeclaringClass().getName(), de.getName(), i);
 		}
 	    } else { // reference types
-		buildString("  if (_$$_t.%s == null) {\n", de.getName());
+		buildString("  if (%s.readPrivateField(_$$_t, %s.class, \"%s\") == null) {\n",
+			DefaultBuildContext.class.getName(), de.getField().getDeclaringClass().getName(), de.getName());
 		if (de.isNotNullable()) {
 		    buildString("    throw new %s();\n", MessageTypeException.class.getName());
 		} else {
@@ -123,11 +124,11 @@ public class DefaultBuildContext extends BuildContext<FieldEntry> {
 		buildString("  } else {\n");
 		if (!isPrivate) {
 		    buildString("    templates[%d].write($1, _$$_t.%s);\n", i, de.getName());
-		    buildString("  }\n");
 		} else {
-		    buildString("    %s.writePrivateField($1, _$$_t, \"%s\", templates[%d]);\n",
-			    DefaultBuildContext.class.getName(), de.getName(), i);
+		    buildString("    %s.writePrivateField($1, _$$_t, %s.class, \"%s\", templates[%d]);\n",
+			    DefaultBuildContext.class.getName(), de.getField().getDeclaringClass().getName(), de.getName(), i);
 		}
+		buildString("  }\n");
 	    }
 	}
 
@@ -136,15 +137,35 @@ public class DefaultBuildContext extends BuildContext<FieldEntry> {
 	return getBuiltString();
     }
 
-    public static void writePrivateField(Packer packer, Object target, String fieldName, Template tmpl) {
+    public static Object readPrivateField(Object target, Class targetClass, String fieldName) {
+	Field field = null;
 	try {
-	    Field field = target.getClass().getDeclaredField(fieldName);
+	    field = targetClass.getDeclaredField(fieldName);
+	    field.setAccessible(true);
+	    Object valueReference = field.get(target);
+	    return valueReference;
+	} catch (Exception e) {
+	    throw new MessageTypeException(e);
+	} finally {
+	    if (field != null) {
+		field.setAccessible(false);
+	    }
+	}
+    }
+
+    public static void writePrivateField(Packer packer, Object target, Class targetClass, String fieldName, Template tmpl) {
+	Field field = null;
+	try {
+	    field = targetClass.getDeclaredField(fieldName);
 	    field.setAccessible(true);
 	    Object valueReference = field.get(target);
 	    tmpl.write(packer, valueReference);
-	    field.setAccessible(false);
 	} catch (Exception e) {
 	    throw new MessageTypeException(e);
+	} finally {
+	    if (field != null) {
+		field.setAccessible(false);
+	    }
 	}
     }
 
@@ -185,16 +206,16 @@ public class DefaultBuildContext extends BuildContext<FieldEntry> {
 		if (!isPrivate) {
 		    buildString("    _$$_t.%s = $1.%s();\n", de.getName(), primitiveReadName(type));
 		} else {
-		    buildString("    %s.readPrivateField($1, _$$_t, \"%s\", templates[%d]);\n",
-			    DefaultBuildContext.class.getName(), de.getName(), i);
+		    buildString("    %s.readPrivateField($1, _$$_t, %s.class, \"%s\", templates[%d]);\n",
+			    DefaultBuildContext.class.getName(), de.getField().getDeclaringClass().getName(), de.getName(), i);
 		}
 	    } else {
 		if (!isPrivate) {
 		    buildString("    _$$_t.%s = (%s) this.templates[%d].read($1, _$$_t.%s);\n",
 			    de.getName(), de.getJavaTypeName(), i, de.getName());
 		} else {
-		    buildString("    %s.readPrivateField($1, _$$_t, \"%s\", templates[%d]);\n",
-			    DefaultBuildContext.class.getName(), de.getName(), i);
+		    buildString("    %s.readPrivateField($1, _$$_t, %s.class, \"%s\", templates[%d]);\n",
+			    DefaultBuildContext.class.getName(), de.getField().getDeclaringClass().getName(), de.getName(), i);
 		}
 	    }
 
@@ -210,18 +231,22 @@ public class DefaultBuildContext extends BuildContext<FieldEntry> {
 	return getBuiltString();
     }
 
-    public static void readPrivateField(Unpacker unpacker, Object target, String fieldName, Template tmpl) {
+    public static void readPrivateField(Unpacker unpacker, Object target, Class targetClass, String fieldName, Template tmpl) {
+	Field field = null;
 	try {
-	    Field field = target.getClass().getDeclaredField(fieldName);
+	    field = targetClass.getDeclaredField(fieldName);
 	    field.setAccessible(true);
 	    Object fieldReference = field.get(target);
 	    Object valueReference = tmpl.read(unpacker, fieldReference);
 	    if (valueReference != fieldReference) {
 		field.set(target, valueReference);
 	    }
-	    field.setAccessible(false);
 	} catch (Exception e) {
 	    throw new MessageTypeException(e);
+	} finally {
+	    if (field != null) {
+		field.setAccessible(false);
+	    }
 	}
     }
 
--- src/test/java/org/msgpack/testclasses/ModifiersFieldsClass.java
+++ src/test/java/org/msgpack/testclasses/ModifiersFieldsClass.java
@@ -15,6 +15,14 @@ public class ModifiersFieldsClass {
 
     int f3;
 
+    public String f4;
+
+    private String f5;
+
+    protected String f6;
+
+    String f7;
+
     public ModifiersFieldsClass() {}
 
     public int getF0() {
@@ -41,6 +49,38 @@ public class ModifiersFieldsClass {
         this.f3 = f3;
     }
 
+    public String getF4() {
+	return f4;
+    }
+
+    public void setF4(String f4) {
+	this.f4 = f4;
+    }
+
+    public String getF5() {
+	return f5;
+    }
+
+    public void setF5(String f5) {
+	this.f5 = f5;
+    }
+
+    public String getF6() {
+	return f6;
+    }
+
+    public void setF6(String f6) {
+	this.f6 = f6;
+    }
+
+    public String getF7() {
+	return f7;
+    }
+
+    public void setF7(String f7) {
+	this.f7 = f7;
+    }
+
     @Override
     public boolean equals(Object o) {
 	if (! (o instanceof ModifiersFieldsClass)) {
@@ -57,6 +97,38 @@ public class ModifiersFieldsClass {
 	    return false;
 	}
 	if (f3 != that.f3) {
+	    return false;
+	}
+	if (f4 == null) {
+	    if (that.f4 != null) {
+		return false;
+	    }
+	}
+	if (that.f4 != null && ! f4.equals(that.f4)) {
+	    return false;
+	}
+	if (f5 == null) {
+	    if (that.f5 != null) {
+		return false;
+	    }
+	}
+	if (that.f5 != null && ! f5.equals(that.f5)) {
+	    return false;
+	}
+	if (f6 == null) {
+	    if (that.f6 != null) {
+		return false;
+	    }
+	}
+	if (that.f6 != null && ! f6.equals(that.f6)) {
+	    return false;
+	}
+	if (f7 == null) {
+	    if (that.f7 != null) {
+		return false;
+	    }
+	}
+	if (that.f7 != null && ! f7.equals(that.f7)) {
 	    return false;
 	}
 	return true;
