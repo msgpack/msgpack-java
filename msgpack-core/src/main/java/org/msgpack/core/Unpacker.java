e0b49b36c184b70de9823d5e217b9b6aa7ea692d
Taro L. Saito <leo@xerial.org>
2014-05-04 01:16:43 -0700

    unpacker option & states

--- msgpack-core/src/main/java/org/msgpack/core/Unpacker.java
+++ msgpack-core/src/main/java/org/msgpack/core/Unpacker.java
@@ -19,6 +19,8 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 class ExtendedTypeHeader {
     private final int type;
@@ -37,51 +39,135 @@ class ExtendedTypeHeader {
     }
 }
 
-public interface Unpacker extends Closeable {
+// TODO impl
+// TODO rename Unpacker -> MessageUnpacker
+public class Unpacker extends Closeable {
 
-    public ValueType getNextType() throws IOException;
+    public static class Options {
+        // allow unpackBinaryHeader to read str format family  // default:true
+        // allow unpackRawStringHeader and unpackString to read bin format family // default: true
+        // string decode malformed input action  // default:report
+        // string decode unmappable character action  // default:report
 
-    public MessageFormat getNextFormat() throws IOException;
+        // unpackString size limit // default: Integer.MAX_VALUE
+    }
+
+    //
+    private static final byte HEAD_BYTE_NEVER_USED_TYPE = (byte) 0xc1;
+
+    private static final byte REQUIRE_TO_READ_HEAD_BYTE = HEAD_BYTE_NEVER_USED_TYPE;
+    private static final int REQUIRE_TO_READ_SIZE = -1;
+
+    private static Charset UTF_8 = Charset.forName("UTF-8");
+
+    private final MessageBufferInput in;
+
+    private MessageBuffer buffer;
+    private int position;
+
+    // For storing data at the buffer boundary (except in unpackString)
+    private MessageBuffer extraBuffer;
+    private int extraPosition;
+
+    // For decoding String in unpackString
+    private CharsetDecoder decorder;
+    private int stringLength;
+
+    // internal states
+    private byte head;
+
+    public Unpacker(MessageBufferInput in) {
+        this.in = in;
+    }
+
+    public ValueType getNextType() throws IOException {
+        // TODO
+        return null;
+    }
+
+    public MessageFormat getNextFormat() throws IOException {
+        return null;
+    }
 
 
-    public void skipToken() throws IOException;
+    public void skipToken() throws IOException {
 
-    public boolean trySkipNil() throws IOException;
+    }
 
-    public void unpackNil() throws IOException;
+    public boolean trySkipNil() throws IOException {
 
-    public boolean unpackBoolean() throws IOException;
+    }
 
-    public byte unpackByte() throws IOException;
+    public void unpackNil() throws IOException {
 
-    public short unpackShort() throws IOException;
+    }
 
-    public int unpackInt() throws IOException;
+    public boolean unpackBoolean() throws IOException {
 
-    public long unpackLong() throws IOException;
+    }
 
-    public BigInteger unpackBigInteger() throws IOException;
+    public byte unpackByte() throws IOException {
 
-    public float unpackFloat() throws IOException;
+    }
 
-    public double unpackDouble() throws IOException;
+    public short unpackShort() throws IOException {
 
-    public String unpackString() throws IOException;
+    }
+
+    public int unpackInt() throws IOException {
+
+    }
+
+    public long unpackLong() throws IOException {
+
+    }
+
+    public BigInteger unpackBigInteger() throws IOException {
+
+    }
+
+    public float unpackFloat() throws IOException {
+
+    }
+
+    public double unpackDouble() throws IOException {
+
+    }
+
+    public String unpackString() throws IOException {
+        // unpackRawStringHeader
+        // ..
+    }
 
 
-    public int unpackArrayHeader() throws IOException;
+    public int unpackArrayHeader() throws IOException {
 
-    public int unpackMapHeader() throws IOException;
+    }
 
-    public ExtendedTypeHeader unpackExtendedTypeHeader() throws IOException;
+    public int unpackMapHeader() throws IOException {
 
-    public int unpackRawStringHeader() throws  IOException;
-    public int unpackBinaryHeader() throws IOException;
+    }
 
-    public void readPayload(ByteBuffer dst) throws IOException;
-    public void readPayload(byte[] dst, int off, int len) throws IOException;
+    public ExtendedTypeHeader unpackExtendedTypeHeader() throws IOException {
 
-    // returns a buffer reference to the payload (zero-copy)
+    }
+
+    public int unpackRawStringHeader() throws IOException {
+
+    }
+    public int unpackBinaryHeader() throws IOException {
+
+    }
+
+    public void readPayload(ByteBuffer dst) throws IOException {
+
+    }
+
+    public void readPayload(byte[] dst, int off, int len) throws IOException {
+
+    }
+
+    // TODO returns a buffer reference to the payload (zero-copy)
     // public long readPayload(...)
 
 }
