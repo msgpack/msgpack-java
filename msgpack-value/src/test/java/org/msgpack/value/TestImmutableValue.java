1bec1c0a9dc3fb83bfff9b30933581f40c320d7c
Tsuyoshi Ozawa <ozawa.tsuyoshi@gmail.com>
2014-05-04 02:37:47 -0700

    Add test for isXxxValue
    Signed-off-by: Tsuyoshi Ozawa <ozawa.tsuyoshi@gmail.com>

--- msgpack-value/src/test/java/org/msgpack/value/TestImmutableValue.java
+++ msgpack-value/src/test/java/org/msgpack/value/TestImmutableValue.java
@@ -22,89 +22,140 @@ import org.msgpack.core.ValueType;
 import org.msgpack.value.impl.ImmutableArrayMapValueImpl;
 
 import java.util.Collections;
-import java.util.EnumSet;
 
 public final class TestImmutableValue {
 
     @Test
     public void testNilValue() {
         Value v = ValueFactory.createNilValue();
-        assertValueType(ValueType.NIL, v.getType());
+        Assert.assertEquals(ValueType.NIL, v.getType());
         Assert.assertTrue(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     @Test
     public void testBooleanValue() {
         Value v = ValueFactory.createBooleanValue(false);
-        assertValueType(ValueType.BOOLEAN, v.getType());
+        Assert.assertEquals(ValueType.BOOLEAN, v.getType());
+        Assert.assertFalse(v.isNilValue());
         Assert.assertTrue(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     @Test
     public void testIntegerValue() {
         Value v = ValueFactory.createIntegerValue(10);
-        assertValueType(ValueType.INTEGER, v.getType());
+        Assert.assertEquals(ValueType.INTEGER, v.getType());
+        Assert.assertFalse(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
         Assert.assertTrue(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     @Test
     public void testFloatValue() {
         Value v = ValueFactory.createFloatValue(0.1f);
-        assertValueType(ValueType.FLOAT, v.getType());
+        Assert.assertEquals(ValueType.FLOAT, v.getType());
+        Assert.assertFalse(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
         Assert.assertTrue(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     @Test
     public void testStringValue() {
         Value v = ValueFactory.createStringValue("msgpack");
-        assertValueType(ValueType.STRING, v.getType());
+        Assert.assertEquals(ValueType.STRING, v.getType());
+        Assert.assertFalse(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
         Assert.assertTrue(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     @Test
     public void testBinaryValue() {
         Value v = ValueFactory.createBinaryValue("msgpack".getBytes());
-        assertValueType(ValueType.BINARY, v.getType());
+        Assert.assertEquals(ValueType.BINARY, v.getType());
+        Assert.assertFalse(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
         Assert.assertTrue(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     @Test
     public void testArrayValue() {
         Value v = ValueFactory.createArrayValue(Collections.<Value>emptyList());
-        assertValueType(ValueType.ARRAY, v.getType());
+        Assert.assertEquals(ValueType.ARRAY, v.getType());
+        Assert.assertFalse(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertTrue(v.isArrayValue());
+        Assert.assertFalse(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
+
         Assert.assertTrue("getType should return Array", v.isArrayValue());
         ArrayValue arrayValue = (ArrayValue) v;
         Assert.assertTrue(arrayValue.isEmpty());
         Assert.assertEquals("size should be zero", 0, arrayValue.size());
-        Assert.assertTrue(v.isArrayValue());
     }
 
     @Test
     public void testMapValue() {
         Value v = ValueFactory.createMapValue(ImmutableArrayMapValueImpl.getEmptyMapInstance());
-        assertValueType(ValueType.MAP, v.getType());
+        Assert.assertEquals(ValueType.MAP, v.getType());
+        Assert.assertFalse(v.isNilValue());
+        Assert.assertFalse(v.isBooleanValue());
+        Assert.assertFalse(v.isIntegerValue());
+        Assert.assertFalse(v.isFloatValue());
+        Assert.assertFalse(v.isStringValue());
+        Assert.assertFalse(v.isBinaryValue());
+        Assert.assertFalse(v.isArrayValue());
         Assert.assertTrue(v.isMapValue());
+        Assert.assertFalse(v.isExtendedValue());
     }
 
     // TODO: Add test after implementing ExtendedValue
     //@Test
     //public void testExtendedValue() {
     //    Value v = ValueFactory.createExtendedValue();
-    //    assertValueType(ValueType.EXTENDED, v.getType());
+    //    assertValueConditionl(ValueType.EXTENDED, v.getType());
     //}
 
-    public void assertValueType(ValueType expected, ValueType actual) {
-        Assert.assertEquals("getType should return " + expected,
-                expected, actual);
-
-        Assert.assertEquals(expected.isBooleanType(), actual.isBooleanType());
-        Assert.assertEquals(expected.isIntegerType(), actual.isIntegerType());
-        Assert.assertEquals(expected.isFloatType(), actual.isFloatType());
-        Assert.assertEquals(expected.isStringType(), actual.isStringType());
-        Assert.assertEquals(expected.isBinaryType(), actual.isBinaryType());
-        Assert.assertEquals(expected.isArrayType(), actual.isArrayType());
-        Assert.assertEquals(expected.isMapType(), actual.isMapType());
-        Assert.assertEquals(expected.isExtendedType(), actual.isExtendedType());
-    }
 
 }
