891fdb64ae9e728a86ca0cbab27c540672a3f6e3
Taro L. Saito <leo@xerial.org>
2014-05-06 13:39:40 -0700

    Add InputStream and WritableByteChannel wrappers

--- msgpack-core/src/main/java/org/msgpack/core/MessageBufferOutput.java
+++ msgpack-core/src/main/java/org/msgpack/core/MessageBufferOutput.java
@@ -1,6 +1,12 @@
 package org.msgpack.core;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
 
 /**
  * Provides a sequence of MessageBuffers for packing the input data
@@ -15,7 +21,7 @@ public interface MessageBufferOutput {
 //    public MessageBuffer next() throws IOException;
 
 
-    public void flush(MessageBuffer buf, int offset, int len);
+    public void flush(MessageBuffer buf, int offset, int len) throws IOException;
 
     /**
      * Flush and close this buffer.
@@ -24,6 +30,64 @@ public interface MessageBufferOutput {
     public void close() throws IOException;
 
 
+}
 
 
+class MessageBufferOutputStream implements MessageBufferOutput {
+
+    private final OutputStream out;
+
+    MessageBufferOutputStream(OutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void flush(MessageBuffer buf, int offset, int len) throws IOException {
+        assert(offset + len < buf.size());
+
+        if(out instanceof FileOutputStream) {
+            FileOutputStream fos = (FileOutputStream) out;
+            FileChannel channel = fos.getChannel();
+            ByteBuffer bb = buf.toByteBuffer(offset, len);
+            channel.write(bb);
+        }
+        else {
+            // TODO reuse the allocated buffer
+            byte[] in = new byte[len];
+            buf.getBytes(offset, in, 0, len);
+            out.write(in, 0, len);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            out.flush();
+        }
+        finally {
+            out.close();
+        }
+    }
+}
+
+
+class MessageBufferOutputChannel implements MessageBufferOutput {
+
+    private final WritableByteChannel channel;
+
+    MessageBufferOutputChannel(WritableByteChannel channel) {
+        this.channel = channel;
+    }
+
+    @Override
+    public void flush(MessageBuffer buf, int offset, int len) throws IOException {
+        assert(offset + len < buf.size());
+        ByteBuffer bb = buf.toByteBuffer(offset, len);
+        channel.write(bb);
+    }
+
+    @Override
+    public void close() throws IOException {
+        channel.close();
+    }
 }
