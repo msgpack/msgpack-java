5d30d6fcbd123745981c4331f978f3656ed2fbdf
Taro L. Saito <leo@xerial.org>
2014-05-20 16:45:02 +0900

    Implement hashCode and equals in ExtendedTypeHeader

--- msgpack-core/src/main/java/org/msgpack/core/ExtendedTypeHeader.java
+++ msgpack-core/src/main/java/org/msgpack/core/ExtendedTypeHeader.java
@@ -6,6 +6,7 @@ package org.msgpack.core;
 public class ExtendedTypeHeader {
     private final int type;
     private final int length;
+
     ExtendedTypeHeader(int type, int length) {
         this.type = type;
         this.length = length;
@@ -17,5 +18,19 @@ public class ExtendedTypeHeader {
 
     public int getLength() {
         return length;
+    }
+
+    @Override
+    public int hashCode() {
+        return (type + 31) * 31 + length;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ExtendedTypeHeader) {
+            ExtendedTypeHeader other = (ExtendedTypeHeader) obj;
+            return this.type == other.type && this.length == other.length;
+        }
+        return false;
     }
 }
