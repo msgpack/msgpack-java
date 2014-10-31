046a2bbea3a6d2417bfcb950473dacefb9c1abbb
Taro L. Saito <leo@xerial.org>
2014-10-31 15:22:27 +0900

    Removed code that use airlift.slice, which cannot be used with JDK6

--- project/Build.scala
+++ project/Build.scala
@@ -135,7 +135,6 @@ object Build extends Build {
       "org.scalacheck" % "scalacheck_2.11" % "1.11.4" % "test",
       "org.xerial" % "xerial-core" % "3.3.0" % "test",
       "org.msgpack" % "msgpack" % "0.6.9" % "test",
-      "io.airlift" % "slice" % "0.6" % "test",
       "com.novocode" % "junit-interface" % "0.10" % "test"
     )
   }
