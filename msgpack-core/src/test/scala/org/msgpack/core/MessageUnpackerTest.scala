ac1ad7e8d8cb86432a85e2eaa44922bb578e8e28
Mitsunori Komatsu <komamitsu@gmail.com>
2015-10-24 22:08:49 +0900

    Take care of incomplete multi-bytes at the tail of buffer

--- msgpack-core/src/test/scala/org/msgpack/core/MessageUnpackerTest.scala
+++ msgpack-core/src/test/scala/org/msgpack/core/MessageUnpackerTest.scala
@@ -16,6 +16,7 @@
 package org.msgpack.core
 
 import java.io._
+import java.nio.ByteBuffer
 
 import org.msgpack.core.buffer._
 import org.msgpack.value.ValueType
@@ -661,6 +662,31 @@ class MessageUnpackerTest extends MessagePackSpec {
         unpacker.unpackInt shouldBe 1
 
         unpacker.getTotalReadBytes shouldBe arr.length
+      }
+    }
+
+    "unpack string crossing end of buffer" in {
+      def check(expected: String, strLen: Int) = {
+        val bytes = new Array[Byte](strLen)
+        val out = new ByteArrayOutputStream
+
+        val packer = MessagePack.newDefaultPacker(out)
+        packer.packBinaryHeader(bytes.length)
+        packer.writePayload(bytes)
+        packer.packString(expected)
+        packer.close
+
+        val unpacker = new MessageUnpacker(new InputStreamBufferInput(new ByteArrayInputStream(out.toByteArray)))
+        val len = unpacker.unpackBinaryHeader
+        unpacker.readPayload(len)
+        val got = unpacker.unpackString
+        unpacker.close
+
+        got shouldBe expected
+      }
+
+      Seq("あ", "aあ", "あa", "あいうえおかきくけこさしすせそ").foreach { s =>
+        Seq(8185, 8186, 8187, 8188, 16377, 16378, 16379, 16380).foreach { n => check(s, n)}
       }
     }
   }
