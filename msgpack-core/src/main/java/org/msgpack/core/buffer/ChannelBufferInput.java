60ddba3cf6ec2f14bf2555f2f122892c644ad38c
Mitsunori Komatsu <komamitsu@gmail.com>
2017-06-23 00:45:06 +0900

    Fix a bug ChannelBufferInput#next blocks until the buffer is filled

--- msgpack-core/src/main/java/org/msgpack/core/buffer/ChannelBufferInput.java
+++ msgpack-core/src/main/java/org/msgpack/core/buffer/ChannelBufferInput.java
@@ -62,11 +62,9 @@ public class ChannelBufferInput
             throws IOException
     {
         ByteBuffer b = buffer.sliceAsByteBuffer();
-        while (b.remaining() > 0) {
-            int ret = channel.read(b);
-            if (ret == -1) {
-                break;
-            }
+        int ret = channel.read(b);
+        if (ret == -1) {
+            return null;
         }
         b.flip();
         return b.remaining() == 0 ? null : buffer.slice(0, b.limit());
